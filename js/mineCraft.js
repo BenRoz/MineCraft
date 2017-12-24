9c3e5903e43258fab9a6cea9a19fda7c569a2a6a
benRoz <bensroz@gmail.com>
2017-12-24 22:19:12 +0200

    sidebar started

--- js/mineCraft.js
+++ js/mineCraft.js
@@ -16,12 +16,32 @@ for (var y=0; y<15; y++){
         line.addClass("row");
         $(".board").append(line);
         line.attr("id","row"+ y);
-
     for (var x=0; x<15; x++){
         var row = $("<div/>");
         row.addClass("square");
         $("#row" + y).append(row);
         row.attr("id","row"+ y + "column" + x);
-
+        row.on("click", squarePress);
     }
 }
+
+function squarePress(){
+    console.log("chicken fucker");
+}
+
+var sideBarVar = $(".sideBar");
+sideBarVar.css({"display":"flex",'height':'600px',"justify-content":"center", 'width':'250px',"background-color":"black"});
+
+var topSideBar = $("<div/>");
+$(".sideBar").append(topSideBar);
+topSideBar.css({"display":"flex","flex-direction":"column","height":"350px","align-items":"center","justify-content":"space-around"});
+for (var w = 0; w < 3; w++){
+    var button = $("<button/>");
+
+    topSideBar.append(button);
+}
+
+
+
+var bottomSideBar = $("<div/>");
+$(".sideBar").append(bottomSideBar);

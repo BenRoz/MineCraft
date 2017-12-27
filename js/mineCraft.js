aa8168c98dd6b93eb17a9c2b2eccc8abc21bd791
benRoz <bensroz@gmail.com>
2017-12-27 21:38:05 +0200

    everything is hundred percent

--- js/mineCraft.js
+++ js/mineCraft.js
@@ -24,50 +24,56 @@ for (var y=0; y<10; y++){
 }
 //on click function
 function squarePress(){
-    console.log($(this));
     var row = $(this).data("row");
     var column = $(this).data("column");
     console.log( column+ ","+ row);
+
     if (($(this).attr("class") == "square dirt") && ($("#shovel").attr("class") == "tool selectedTool")){
         $(this).attr("class", "square");
-        settingCounter.dirt++;
-        $("#dirt").text(settingCounter.dirt);
+        settingCounter.dirtBut++;
+        $("#dirtBut").text(settingCounter.dirtBut);
     }
     else if (($(this).attr("class") == "square dirtGrass")&& ($("#shovel").attr("class") == "tool selectedTool"))  {
-         $(this).attr("class", "square");
-         settingCounter.dirtGrass++;
-         $("#dirtGrass").text(settingCounter.dirtGrass);
+          $(this).attr("class", "square");
+         settingCounter.dirtGrassBut++;
+         $("#dirtGrassBut").text(settingCounter.dirtGrassBut);
     }
 
     else if (($(this).attr("class") == "leaf") && ($("#axe").attr("class") == "tool selectedTool")){
         $(this).css({"visibility":"hidden"})
-        settingCounter.leaves++;
-        $("#leaves").text(settingCounter.leaves);
+        settingCounter.leafBut++;
+        $("#leafBut").text(settingCounter.leafBut);
     }
     else if (($(this).attr("class") == "trunck") && ($("#axe").attr("class") == "tool selectedTool")){
-         $(this).css({"visibility":"hidden"})
-         settingCounter.wood++;
-         $("#wood").text(settingCounter.wood);
+          $(this).css({"visibility":"hidden"})
+         settingCounter.trunckBut++;
+         $("#trunckBut").text(settingCounter.trunckBut);
     }
     else if (($(this).attr("class")== "rock") && ($("#pickaxe").attr("class") == "tool selectedTool")){
         $(this).css({"visibility":"hidden"});
-        settingCounter.stones++;
-        $("#stones").text(settingCounter.stones);
+        settingCounter.rockBut++;
+        $("#rockBut").text(settingCounter.rockBut);
     }
     else if (($(this).attr("class")== "Ariel")){
          $(this).css({"visibility":"hidden"});
-        settingCounter.Ariel++;
-        $("#stones").text(settingCounter.Ariel);
+        settingCounter.ArielBut++;
+        $("#ArielBut").text(settingCounter.ArielBut);
     }
+    if (!$('#pickaxe').attr("class").includes("selectedTool")&&!$('#axe').attr("class").includes("selectedTool")&&
+    !$('#shovel').attr("class").includes("selectedTool")){
+        placingOnBoard($(this));
+    }
+
+    console.log($(this).css("background-image"))
 }
 //setting inventory values
 var settingCounter ={
-stones: "0",
-leaves : "0",
-wood: "0",
-dirtGrass: "0",
-dirt: "0",
-Ariel: "0",
+rockBut: 2,
+leafBut : 0,
+trunckBut: 1,
+dirtGrassBut: 2,
+dirtBut: "0",
+ArielBut: 5,
 }
 
 //creating sidebar for buttons
@@ -86,33 +92,68 @@ for (var w = 0; w < 3; w++){
         $('.tool').removeClass("selectedTool");
         $('.inventory').removeClass("selectedTool");
         $(this).addClass("selectedTool");
+
     })
     topSideBar.append(buttonsUp);
 }
+
 $("#axe").html("Axe");
 $("#pickaxe").html("Pickaxe");
 $("#shovel").html("Shovel");
+//inventory
 
 var bottomSideBar = $("<div/>");
 $(".sideBar").append(bottomSideBar);
 bottomSideBar.css({"display":"flex","flex-direction":"row","flex-wrap": "wrap","height":"250px","align-items":"space-around","padding-top":"30px","justify-content":"space-around"});
-var inventoryArray = ["dirt","dirtGrass", "stones", "leaves", "wood","Ariel"];
+
+//creating inventory
+var selectedInventory;
+var tempImage;
+
+var matchingIdToClass= {
+dirtBut : "square dirt",
+dirtGrassBut : "square dirtGrass",
+rockBut : "rock",
+leafBut: "leaf",
+trunckBut: "trunck",
+ArielBut : "Ariel"
+}
+
+var tempClassName="";
+var inventoryArray = ["dirtBut","dirtGrassBut", "rockBut", "leafBut", "trunckBut","ArielBut"];
 for (var z = 0; z < inventoryArray.length; z++){
        var buttonsDown = $("<button/>");
         buttonsDown.attr("id", inventoryArray[z]);
         buttonsDown.addClass('inventory');
-        buttonsDown.text(0);
+        buttonsDown.text(settingCounter[inventoryArray[z]]);
+
         buttonsDown.click(function(){
         $('.inventory').removeClass("selectedTool");
         $('.tool').removeClass("selectedTool");
         $(this).addClass("selectedTool");
+//        tempImage = $(this).css("background-image");
+        tempClassName= $(this).attr("id");
+
+ console.log(matchingIdToClass[tempClassName]);
     })
          bottomSideBar.append(buttonsDown);
 }
 
+//
+function placingOnBoard(a){
 
+    if(settingCounter[tempClassName] > 0){
+        if (a.css('background-image') == "none"){
+        a.attr("class", matchingIdToClass[tempClassName]);
+         console.log(matchingIdToClass[tempClassName]);
+         console.log(matchingIdToClass[tempClassName]);
+         settingCounter[tempClassName]--;
+         $("#"+tempClassName).text(settingCounter[tempClassName]);
+         }
 
-//Making Tree
+     }
+}
+//creating Tree
 var treeDivContainer = $('<div/>');
 treeDivContainer.attr('id','tree');
 treeDivContainer.appendTo($('.main'));
@@ -151,24 +192,6 @@ for (var r=0; r<3; r++){
   } );
 
 
-
-function tree(){
-    this.id =
-    row (y) + line (x) . addClass(tree);
-    row (y) + line (x-1) . addClass(tree);
-    row (y) + line (x-2) . addClass(tree);
-    row (y) + line (x-3) . addClass(leaves);
-    row (y) + line (x-4) . addClass(leaves);
-    row (y) + line (x-5) . addClass(leaves);
-    row (y-1) + line (x-3) . addClass(leaves);
-    row (y-1) + line (x-4) . addClass(leaves);
-    row (y-1) + line (x-5) . addClass(leaves);
-    row (y+1) + line (x-3) . addClass(leaves);
-    row (y+1) + line (x-4) . addClass(leaves);
-    row (y+1) + line (x-5) . addClass(leaves);
-
-
-}
 //creating rocks
 var rocks = $("<div/>");
 rocks.attr("id","rocks");
@@ -190,8 +213,7 @@ $( function() {
     });
   } );
 
-<<<<<<< HEAD
-=======
+
 //create cloud
 function createCloud(){
     var x = 3;
@@ -202,51 +224,7 @@ function createCloud(){
     $('#column'+(x-1)+'row'+(y+1)).addClass('cloud');
     $('#column'+(x-1)+'row'+(y)).addClass('cloud');
     $('#column'+(x-2)+'row'+(y+1)).addClass('cloud');
+    $('#column'+(x-1)+'row'+(y+2)).addClass('cloud');
 }
 createCloud();
->>>>>>> 6ce04ec52fbda18d7e110685efdda10621537ddd
 
-
-// function setTree(){
-//     if ($(this).attr("background-image") != 'url()'){
-//         var y = $(this).data("row");
-//         var x = $(this).data("column");
-//         if (y<7){
-//         //trunk
-//             $('#column'+x+'row'+y).css({"background-image":"url('./images/tree.png')"});
-//             $('#column'+x+'row'+(y-1)).css({"background-image":"url('./images/tree.png')"});
-//             $('#column'+x+'row'+(y-2)).css({"background-image":"url('./images/tree.png')"});
-//             //leaves
-//             $('#column'+x+'row'+(y-3)).css({"background-image":"url('./images/leaf.png')"});
-//             $('#column'+x+'row'+(y-4)).css({"background-image":"url('./images/leaf.png')"});
-//             $('#column'+x+'row'+(y-5)).css({"background-image":"url('./images/leaf.png')"});
-//             $('#column'+(x-1)+'row'+(y-3)).css({"background-image":"url('./images/leaf.png')"});
-//             $('#column'+(x-1)+'row'+(y-4)).css({"background-image":"url('./images/leaf.png')"});
-//             $('#column'+(x-1)+'row'+(y-5)).css({"background-image":"url('./images/leaf.png')"});
-//             $('#column'+(x+1)+'row'+(y-3)).css({"background-image":"url('./images/leaf.png')"});
-//             $('#column'+(x+1)+'row'+(y-4)).css({"background-image":"url('./images/leaf.png')"});
-//             $('#column'+(x+1)+'row'+(y-5)).css({"background-image":"url('./images/leaf.png')"});
-//         }else if (y>7){
-//             continue;
-//         }
-//     }
-// }
-// function backtoSquare(){
-//     var y = $(this).data("row");
-//     var x = $(this).data("column");
-    
-//     $('#column'+x+'row'+y).css({"background-image":""});
-//     $('#column'+x+'row'+(y-1)).css({"background-image":""});
-//     $('#column'+x+'row'+(y-2)).css({"background-image":""});
-//     //leaves
-//     $('#column'+x+'row'+(y-3)).css({"background-image":""});
-//     $('#column'+x+'row'+(y-4)).css({"background-image":""});
-//     $('#column'+x+'row'+(y-5)).css({"background-image":""});
-//     $('#column'+(x-1)+'row'+(y-3)).css({"background-image":""});
-//     $('#column'+(x-1)+'row'+(y-4)).css({"background-image":""});
-//     $('#column'+(x-1)+'row'+(y-5)).css({"background-image":""});
-//     $('#column'+(x+1)+'row'+(y-3)).css({"background-image":""});
-//     $('#column'+(x+1)+'row'+(y-4)).css({"background-image":""});
-//     $('#column'+(x+1)+'row'+(y-5)).css({"background-image":""});
-    
-// }
